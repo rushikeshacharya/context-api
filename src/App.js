51cd59a87420383cdecd02b64e888f32a5f1ec40
Rushikesh Acharya <v-racharya@microsoft.com>
2023-09-29 18:33:23 +0530

    feat: issue of passing props in multiple components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Child = (props) => {
+  return(
+    <div>
+      <h2>
+        Child: {props.brand}
+      </h2>
     </div>
-  );
+  )
+}
+
+const GrandChild = (props) => {
+  return(
+    <div>
+      <h2>GrandChild: </h2>
+      <Child brand={props.brand} />
+    </div>
+  )
+}
+const App = () => {
+ const [brandname] = useState("Amazon");
+  return (
+    <div>
+      <h1> Hello</h1>
+      {/* <Child brand={brandname} /> */}
+      <GrandChild brand={brandname}/>
+    </div>
+  )
 }
 
 export default App;
